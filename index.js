5ff35620302c166dbcfc4e17162b2b369f6ef169
Shubham Mourya <95007067+ShubhamMoury@users.noreply.github.com>
2023-02-10 10:50:28 +0530

    Added new import api endpoint for TestNG

--- index.js
+++ index.js
@@ -2,38 +2,30 @@ const core = require('@actions/core'); //ES5
 const axios = require('axios').default;
 const FormData = require('form-data');
 
-const vansahToken = core.getInput('vansahToken');
+const vansahConnectToken = core.getInput('vansahConnectToken');
 const testFormat = core.getInput('testFormat');
 const testPaths = core.getInput('testPaths');
-const Issue_TestFolders = core.getInput('Issue_TestFolders');
-const comment = core.getInput('comment');
 
-console.log(vansahToken);
-console.log(testFormat);
-console.log(testPaths);
-console.log(Issue_TestFolders);
-console.log(comment);
+const apiUrl = "";
+const nodeApiVersion = "";
 
 
-// var bodyFormData = new FormData();
-// bodyFormData.append('file', testPaths);
-// bodyFormData.append('log_identifier', log_identifier);
-// bodyFormData.append('result_key', 2);
-// bodyFormData.append('comment', comment);
-// console.log(bodyFormData);
+let bodyFormData = new FormData();
+bodyFormData.append('testFormat', testFormat);
+bodyFormData.append('testPaths', testPaths);
+console.log(bodyFormData);
 
 
 
-
-// axios({
-//     method: "post",
-//     url: "https://apiuat.vansah.online/api/v3/auto/test_case/update_test_log",
-//     data: bodyFormData,
-//     headers: { "vansah-token": "abe63e37-0741-11ed-bdf2-ae827922bf87" , "Content-Type": "multipart/form-data" },
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+axios({
+    method: "post",
+    url: apiUrl+"/api/"+nodeApiVersion+"/testCase/import/XML",
+    data: bodyFormData,
+    headers: { "Authorization": vansahConnectToken , "Content-Type": "multipart/form-data" },
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
